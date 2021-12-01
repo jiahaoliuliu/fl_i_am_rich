d9039e6a499fbdb1f3e7ac6570060e84971aece5
Jiahao Liu Liu <jiahaoliuliu@Jiahaos-MacBook-Pro.local>
2021-12-01 22:22:28 +0400

    Create a specific folder for i_am_rich

---
+++ 3_i_am_rich/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// The main function is the starting point for all our Flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+            title: const Text('I Am Rich'),
+            backgroundColor: Colors.blueGrey[900]),
+        backgroundColor: Colors.blueGrey,
+      ),
+    ),
+  );
+}
--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-void main() => runApp(
-      const MaterialApp(
-        home: Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
