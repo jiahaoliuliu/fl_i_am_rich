44f188f7bf64208c9fad2fc026623dafadb8950a
Jiahao Liu <jiahaoliuliu@gmail.com>
2021-12-03 21:43:20 +0400

    Using listTile for the cards contents

--- 6_mi_card_flutter/lib/main.dart
+++ 6_mi_card_flutter/lib/main.dart
@@ -37,57 +37,44 @@ class MyApp extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              color: Colors.white,
+            Card(
               margin: EdgeInsets.symmetric(
                 vertical: 10.0,
                 horizontal: 25.0,
               ),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.phone,
-                    color: Colors.teal,
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '+971 525 94 89 31',
+                  style: TextStyle(
+                    color: Colors.teal.shade900,
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 15.0,
                   ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    '+971 525 94 89 31',
-                    style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontFamily: 'Source Sans Pro',
-                        fontSize: 20.0),
-                  ),
-                ],
+                ),
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              color: Colors.white,
+            Card(
               margin: EdgeInsets.symmetric(
                 vertical: 10.0,
                 horizontal: 25.0,
               ),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.email,
-                    color: Colors.teal,
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'jiahaoliuliu@gmail.com',
+                  style: TextStyle(
+                    color: Colors.teal.shade900,
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 15.0,
                   ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    'jiahaoliuliu@gmail.com',
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20.0,
-                    ),
-                  )
-                ],
+                ),
               ),
             ),
           ],
@@ -96,3 +83,23 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// Row(
+// children: <Widget>[
+// Icon(
+// Icons.phone,
+// color: Colors.teal,
+// ),
+// SizedBox(
+// width: 10.0,
+// ),
+// Text(
+// '+971 525 94 89 31',
+// style: TextStyle(
+// color: Colors.teal.shade900,
+// fontFamily: 'Source Sans Pro',
+// fontSize: 20.0,
+// ),
+// ),
+// ],
+// ),
