a64d85cd85013263cc62176a678c3d1166d56e17
Jiahao Liu <jiahaoliuliu@gmail.com>
2021-12-03 19:48:32 +0400

    Adding profile images and name

--- 6_mi_card_flutter/lib/main.dart
+++ 6_mi_card_flutter/lib/main.dart
@@ -13,35 +13,15 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                  width: 100.0,
-                  height: double.infinity,
-                  color: Colors.red,
-                ),
-                Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        height: 100.0,
-                        width: 100.0,
-                        color: Colors.yellow,
-                      ),
-                      Container(
-                        height: 100.0,
-                        width: 100.0,
-                        color: Colors.green,
-                      ),
-                    ]),
-                Container(
-                  width: 100.0,
-                  height: double.infinity,
-                  color: Colors.blue,
-                ),
-              ]),
-        ),
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('assets/images/Profile.png'),
+            ),
+            Text('Jiahao Liu'),
+          ],
+        )),
       ),
     );
   }
