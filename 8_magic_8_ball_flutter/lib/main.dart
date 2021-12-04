e29cbbbc3b352fcfb4088a5f7c55c16995d0a68d
Jiahao Liu <jiahaoliuliu@gmail.com>
2021-12-04 18:15:17 +0400

    8. Created a basic 8 ball

--- 8_magic_8_ball_flutter/lib/main.dart
+++ 8_magic_8_ball_flutter/lib/main.dart
@@ -1,7 +1,41 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+            backgroundColor: Colors.blue,
+            appBar: AppBar(
+              title: Text('Ask Me Anything'),
+              backgroundColor: Colors.blueAccent,
+            ),
+            body: BallBody()),
       ),
     );
+
+class BallBody extends StatefulWidget {
+  @override
+  _BallBodyState createState() => _BallBodyState();
+}
+
+class _BallBodyState extends State<BallBody> {
+  int ballNumber = 1;
+
+  void updateBallNumber() {
+    ballNumber = Random().nextInt(5) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+          child: Image.asset('images/ball$ballNumber.png'),
+          onPressed: () {
+            setState(() {
+              updateBallNumber();
+            });
+          }),
+    );
+  }
+}
