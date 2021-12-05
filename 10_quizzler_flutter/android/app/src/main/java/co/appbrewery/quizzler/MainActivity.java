75ee15918db89e7d961ad1b7e41907cbdde516fd
Jiahao Liu <jiahaoliuliu@gmail.com>
2021-12-05 23:54:27 +0400

    10. Added a set of checkers for questions and answers

--- 10_quizzler_flutter/android/app/src/main/java/co/appbrewery/quizzler/MainActivity.java
+++ 10_quizzler_flutter/android/app/src/main/java/co/appbrewery/quizzler/MainActivity.java
@@ -1,6 +1,6 @@
 package co.appbrewery.quizzler;
 
-+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
 }
--- 10_quizzler_flutter/lib/main.dart
+++ 10_quizzler_flutter/lib/main.dart
@@ -25,6 +25,42 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [];
+
+  List<String> questionsList = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+
+  List<bool> answersList = [false, true, true];
+
+  int questionNumber = 0;
+
+  void setScore(bool answer) {
+    if (answer == answersList[questionNumber]) {
+      // Correct
+      scoreKeeper.add(
+        Icon(
+          Icons.check,
+          color: Colors.green,
+        ),
+      );
+    } else {
+      // Incorrect
+      scoreKeeper.add(Icon(
+        Icons.close,
+        color: Colors.red,
+      ));
+    }
+  }
+
+  void showNextQuestionIfAny() {
+    if (questionNumber + 1 < questionsList.length) {
+      questionNumber++;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +73,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questionsList[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -63,6 +99,10 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                setState(() {
+                  setScore(true);
+                  showNextQuestionIfAny();
+                });
               },
             ),
           ),
@@ -83,11 +123,17 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {
+                  setScore(false);
+                  showNextQuestionIfAny();
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
